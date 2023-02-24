80104cdaf41ff71f17c48256f6553db308275a38
Sergei <korolnyy2@mail.ru>
2023-02-24 17:38:12 +0300

    initial

---
+++ src/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import {createRoot} from "react-dom/client";
+import App from "./App";
+
+const root = createRoot(document.getElementById('root'))
+
+window.addEventListener('load', () => {
+    root.render(<App/>)
+})
